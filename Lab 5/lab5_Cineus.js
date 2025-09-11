f2b0467753c47a91cb9f3239b5da8819d2a3822d
Jonathan Cineus <Jonathancineus580@gmail.com>
2025-09-11 15:56:38 -0400

    complete class examples

---
+++ Lab 5/lab5_Cineus.js
@@ -0,0 +1,94 @@
+/**
+ * Jonathan Cineus
+ * lab 5, more about functions, object
+ * sep 11, 2025
+ */
+console.log("Jonathan Cineus")
+// EXAMPLE 1
+// anonymous function in a variable
+let  greet = function(username){
+    console.log(`welcome to function ${username}`)
+}
+// arrow function
+let greeting = function(username){
+    console.log(`Good afternoon ${username}`)
+}
+// EXAMPLE 2: default parameters
+// randomly generated a number between 1 and 9
+// function will pass the amount of time  that the number will be generated
+function rand_number(x=1){
+    for(let n = 1; n <= x; n++){
+        let num = Math.floor(Math.random()*9)
+        console.log(`${num}`) 
+    }
+}
+
+// EXAMPLE 3: spread syntax ...
+number = [2, 0, -10, 5, 8, -9]
+let max_number = Math.max(...numbers)
+console.log(`------- Example 3: Spread ----`)
+console.log(`The maximum number is = ${max_number}`)
+
+// create an object 'car'
+const car ={
+    // properties
+    type: "Fiat",
+    model: "500",
+    color: "white", 
+
+    //method
+    car_description: function(){
+        return `Car type = ${this.type}, ${this.model}, car color ${this.color}`
+    }
+}
+
+// EXAMPLE 5
+// create an object 'myMath' that will calculate the perimeter of a rectangle or the area of a rectangle
+const myMath = {
+    // methods
+    perimeter : function(w=0,l=0){ return (2*w)+(2*1) },
+    area : function(w=0,l=0){return w*1}
+}
+
+// EXAMPLE 6
+const cat = {
+    //properties
+    name:"Mickey",
+    color:"Black with white spots",
+    bred: "unknown",
+
+    // method
+    meow: ()=>{console.log("MEOW MEOW MEOW")}
+}
+
+// EXAMPLE 7
+const hen = {
+    // properties
+    name : "Helen",
+    eggCount : 0,
+
+    // method
+    layAnEgg : ()=>{
+        this.eggCount++; 
+        return `${this.name} lays an egg`
+    },
+    resetEggCount(){
+        this.eggCount = 0
+        return `${this.name} egg count = ${this.eggCount}`
+    }
+
+}
+
+// EXAMPLE 8
+
+function yell(message){
+    try{
+        console.log(message.toUpperCase().repeat(3))
+    }
+    catch(error){
+        console.log(error)
+        console.log('Please pass a string next time!')
+    }   
+}
+
+//EXERCISE 1
