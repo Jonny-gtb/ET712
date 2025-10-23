c9c8f2a895138c7387e49bb9dc4746499ee28d99
Jonny-gtb <jonathancineus580@gmail.com>
2025-10-23 15:45:45 -0400

    Add files via upload

---
+++ Project 1.js
@@ -0,0 +1,9 @@
+const lettersContainer = document.getElementById('letters');
+const animalName = document.getElementById('animal-name');
+const animalImage = document.getElementById('animal-image');
+
+const animals = {
+  A: { name: "Alligator", img: "https://upload.wikimedia.org/wikipedia/commons/5/5e/AmericanAlligator.jpg" },
+  B: { name: "Bear", img: "https://upload.wikimedia.org/wikipedia/commons/7/79/2010-kodiak-bear-1.jpg" },
+  C: { name: "Cat", img: "https://upload.wikimedia.org/wikipedia/commons/a/a3/81_INF_DIV_SSI.jpg" },
+ 
